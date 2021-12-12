4206d7bda4e80a91e52f5c17a18d993beb8d9321
Oggy1923 <oguzhan@heelocoupons.com>
2021-12-12 12:30:27 -0500

    Implemented the PicturesAdapter, this is the adapter for the RecyclerView inside PicturesFragment

---
+++ app/src/main/java/mobi/heelo/traderevchallenge/adapters/PicturesAdapter.kt
@@ -0,0 +1,66 @@
+package mobi.heelo.traderevchallenge.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import mobi.heelo.traderevchallenge.R
+import mobi.heelo.traderevchallenge.models.UnsplashResponseItem
+
+class PicturesAdapter: RecyclerView.Adapter<PicturesAdapter.PictureViewHolder>() {
+
+    inner class PictureViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private val differCallback = object : DiffUtil.ItemCallback<UnsplashResponseItem>() {
+        override fun areItemsTheSame(
+            oldItem: UnsplashResponseItem, newItem: UnsplashResponseItem
+        ): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(
+            oldItem: UnsplashResponseItem, newItem: UnsplashResponseItem
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
+
+    override fun getItemCount(): Int {
+        return differ.currentList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PictureViewHolder {
+        return PictureViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_picture, parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: PictureViewHolder, position: Int) {
+        val currentPicture = differ.currentList[position]
+
+        val photo_item_iv: ImageView = holder.itemView.findViewById<ImageView>(R.id.photo_item_iv)
+
+        holder.itemView.apply {
+            Glide.with(this).load(currentPicture.urls.thumb).into(photo_item_iv)
+
+            setOnClickListener {
+                onItemClickListener?.let { it(currentPicture, position) }
+            }
+        }
+    }
+
+    private var onItemClickListener: ((UnsplashResponseItem, position: Int) -> Unit)? = null
+
+    fun setOnItemClickListener(listener: (UnsplashResponseItem, position: Int) -> Unit) {
+        onItemClickListener = listener
+    }
+
+}
